9608cf9a789d82870bfb12a23db36babe86f6e92
stikkireddy <54602805+stikkireddy@users.noreply.github.com>
2024-10-02 07:40:12 -0400

    fixed README and sql examples (#2)
    Co-authored-by: Sri Tikkireddy <sri.tikkireddy+github@databricks.com>

--- sql/02_encrypt_sql_udf.sql
+++ sql/02_encrypt_sql_udf.sql
@@ -5,12 +5,12 @@ USE CATALOG <catalog>;
 USE SCHEMA <schema>;
 
 -- Create the following UDF to encrypt
-CREATE OR REPLACE FUNCTION encrypt_fpe(text STRING, operation STRING)
+CREATE OR REPLACE FUNCTION encrypt_fpe(text STRING)
 RETURNS STRING
 DETERMINISTIC
 LANGUAGE SQL
 -- you may chose to specify functions from another schema
-RETURN SELECT_encrypt_decrypt_fpe(
+RETURN SELECT _encrypt_decrypt_fpe(
     key => secret(<scope>, <encryption secret key>),
     tweak => secret(<scope>, <tweak secret key>),
     text => text,
